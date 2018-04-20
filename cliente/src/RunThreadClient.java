9b77c7793d0a7f1c57e23578bfd3618da412522e
edsonjunioo <edsonjunio.sousa@gmail.com>
2018-04-20 18:40:43 -0300

    criação de branch development

--- cliente/src/RunThreadClient.java
+++ cliente/src/RunThreadClient.java
@@ -1,2 +1,14 @@
+
+
+
 public class RunThreadClient {
+
+    public static void main(String[] args){
+
+        Cliente cliente = new Cliente();
+        cliente.startclientcomunication();
+
+
+    }
+
 }
--- servidor/src/RunThreadServer.java
+++ servidor/src/RunThreadServer.java
@@ -2,4 +2,10 @@
 
 
 public class RunThreadServer {
+
+    public static void main(String[] args){
+
+
+    }
+
 }
