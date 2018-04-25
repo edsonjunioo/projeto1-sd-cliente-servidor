d70c3bcb051f8490fe3c4d5bdfca89bc28ca36dd
edsonjunioo <edsonjunio.sousa@gmail.com>
2018-04-25 18:22:29 -0300

    Inclusão de maga e chave

--- servidor/src/Server.java
+++ servidor/src/Server.java
@@ -1,13 +1,10 @@
 import java.io.*;
 import java.math.BigInteger;
 import java.net.*;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
-import java.util.Queue;
+import java.nio.file.FileAlreadyExistsException;
+import java.util.*;
 
-public class Server
-{
+public class Server {
     public static Properties getProp() throws IOException {
         Properties props = new Properties();
         FileInputStream file = new FileInputStream("./servidor/properties/dados.properties");
@@ -18,48 +15,60 @@ public class Server
 
 
     public void server() {
-    try {
+        try {
 
 
-        String port;
+            String port;
 
-        Properties prop = getProp();
+            Properties prop = getProp();
 
-        port = prop.getProperty("prop.server.port");
-        int porta = Integer.parseInt(port);
+            port = prop.getProperty("prop.server.port");
+            int porta = Integer.parseInt(port);
 
-        DatagramSocket serverSocket = new DatagramSocket(porta);
-        while (true) {
-            byte[] receiveData = new byte[1400];
-            byte[] sendData = new byte[1400];
-            System.out.println("Server Running");
-            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-            serverSocket.receive(receivePacket);
-            String sentence = new String(receivePacket.getData());
-            System.out.println("Recebido: " + sentence);
-            BigInteger chave = BigInteger.ONE;
-            InetAddress IPAddress = receivePacket.getAddress();
-            int port_defined = receivePacket.getPort();
-            String capitalizedSentence = sentence.toUpperCase();
-            sendData = capitalizedSentence.getBytes();
-            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port_defined);
-            serverSocket.send(sendPacket);
+            DatagramSocket serverSocket = new DatagramSocket(porta);
+            BigInteger LIMIT = new BigInteger("20");
+            BigInteger key = BigInteger.ZERO;
+            Queue<Object> queue = new LinkedList<Object>();
+
+            while (true) {
+                byte[] receiveData = new byte[1400];
+                byte[] sendData = new byte[1400];
+                System.out.println("Server Running");
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                serverSocket.receive(receivePacket);
+                String sentence = new String(receivePacket.getData());
+                //System.out.println("Recebido: " + sentence);
+                InetAddress IPAddress = receivePacket.getAddress();
+                int port_defined = receivePacket.getPort();
+                String capitalizedSentence = sentence.toUpperCase();
+                //sendData = capitalizedSentence.getBytes();
+                //DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port_defined);
+                //serverSocket.send(sendPacket);
+
+                if(key.compareTo(LIMIT) < 0) {
+                    key = key.add(new BigInteger("1"));
+                    Map<BigInteger,String> message = new HashMap<>();
+                    message.put(key,sentence);
+                    System.out.println("Recebido:" + message);
+                    sendData = capitalizedSentence.getBytes();
+                    DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port_defined);
+                    serverSocket.send(sendPacket);
 
 
-            Map<BigInteger, String> mensagem = new HashMap<>();
-            mensagem.put(chave, sentence);
-            System.out.println("Mapa fila :" + mensagem);
 
 
+                } else {
+                    System.out.println("Ultrapassou limite da chave para envio de mensagens");
+                }
+            }
 
-
+        } catch (Exception e) {
+            System.out.println("Erro" + e.getMessage());
         }
 
 
 
-
-        }catch(Exception e){
-        System.out.println("Erro: " + e.getMessage());
-        }
     }
+
+
 }
