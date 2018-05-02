e6ddaf1e742c9a638899382f2305e4274871b71e
edsonjunioo <edsonjunio.sousa@gmail.com>
2018-05-02 19:56:17 -0300

    set size key

--- servidor/src/Server.java
+++ servidor/src/Server.java
@@ -60,7 +60,7 @@ public class Server {
 
             DatagramSocket serverSocket = new DatagramSocket(porta);
             logger.info("Conexão do servidor iniciada");
-            BigInteger LIMIT = new BigInteger("20");
+            BigInteger LIMIT = new BigInteger("2048");
             BigInteger key = BigInteger.ZERO;
             Queue<Object> queue = instaciaFila();
 
