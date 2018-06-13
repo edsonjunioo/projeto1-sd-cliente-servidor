3dea9fbc69926fd055ec6511e3aa77f2077cbc88
edsonjunioo <edsonjunio.sousa@gmail.com>
2018-06-13 17:43:57 -0300

    segunda tentativa client/server multithread

--- servidor/src/Thread1.java
+++ servidor/src/Thread1.java
@@ -17,13 +17,7 @@ public class Thread1 implements Runnable {
 
     Queue<Object> queue = new LinkedList<Object>();
 
-    public Queue<Object> getQueue(){
-        return queue;
-    }
-
-    public void setQueue(Queue<Object> queue){
-        this.queue = queue;
-    }
+    Map<BigInteger, String> map = new HashMap<>();
 
     public static Properties getProp() throws IOException {
         Properties props = new Properties();
@@ -52,6 +46,7 @@ public class Thread1 implements Runnable {
             DatagramSocket serverSocket = new DatagramSocket(porta);
             logger.info("Conexão do servidor iniciada");
 
+
             while (true) {
 
 
@@ -70,17 +65,22 @@ public class Thread1 implements Runnable {
                 //DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port_defined);
                 //serverSocket.send(sendPacket);
 
-                if(sentence.contains("create") || sentence.contains("read") || sentence.contains("update") || sentence.contains("delete")) {
+                if(sentence.contains("create") || sentence.contains("read") || sentence.contains("update") || sentence.contains("delete") || sentence.contains("limpar")) {
                     queue.add(sentence);
                 }
 
                 logger.info(queue);
 
+                String mensagem = ((LinkedList<Object>) queue).getFirst().toString();
 
 
-                Thread2 thread2 = new Thread2(queue);
+
+
+                Thread2 thread2 = new Thread2(mensagem);
                 Thread t2 = new Thread(thread2);
                 t2.start();
+
+                ((LinkedList<Object>) queue).removeFirst();
 
 
 
--- servidor/src/Thread2.java
+++ servidor/src/Thread2.java
@@ -8,70 +8,78 @@ import java.util.Queue;
 
 public class Thread2 extends Thread1 implements Runnable {
 
+    final Logger logger = Logger.getLogger("server");
+
     Queue<Object> queuef2 = new LinkedList<Object>();
 
-    public Thread2(Queue<Object> queue){
-        this.queuef2 = queue;
+    Queue<Object> queuef3 = new LinkedList<Object>();
+
+    String mensagem;
+
+    public Thread2(String mensagem){
+        this.mensagem = mensagem;
     }
 
 
 
-
-    Map<BigInteger, String> map = new HashMap<>();
-
-
     @Override
     synchronized public void run() {
-        final Logger logger = Logger.getLogger("server");
-        logger.info("iniciou processamento Thread 2");
-        logger.info(queuef2);
 
 
+            queuef2.add(mensagem);
+            queuef3.add(mensagem);
 
-        if (((LinkedList<Object>) queuef2).getLast().toString().contains("create")) {
-            String[] url = ((LinkedList<Object>) queuef2).getFirst().toString().split("/");
-
-            int chave = Integer.parseInt(url[1]);
-            BigInteger key1 = BigInteger.valueOf(chave);
-            map.put(key1, url[2]);
-            logger.info(map);
-        }
+            final Logger logger = Logger.getLogger("server");
+            logger.info("Thread 2");
+            logger.info("Log em disco" + queuef2);
 
 
-        if (((LinkedList<Object>) queuef2).getFirst().toString().contains("read")) {
+            logger.info("processamento");
+            if (mensagem.contains("create")) {
+                String[] url = ((LinkedList<Object>) queuef3).getFirst().toString().split("/");
 
-            logger.info(map);
-
-            logger.info(queue);
-
-        }
-
-        if (((LinkedList<Object>) queuef2).getFirst().toString().contains("update")) {
-            String[] url = ((LinkedList<Object>) queuef2).getFirst().toString().split("/");
-
-            int chave = Integer.parseInt(url[1]);
-            BigInteger key1 = BigInteger.valueOf(chave);
-            map.remove(key1);
-            map.put(key1, url[2]);
-        }
+                int chave = Integer.parseInt(url[1]);
+                BigInteger key1 = BigInteger.valueOf(chave);
+                map.put(key1, url[2]);
+                logger.info("Mapa:" + map);
+                logger.info(queuef3);
+            }
 
 
-        if (((LinkedList<Object>) queuef2).getFirst().toString().contains("delete")) {
-            String[] url = ((LinkedList<Object>) queuef2).getFirst().toString().split("/");
+            if (((LinkedList<Object>) queuef3).getFirst().toString().contains("read")) {
 
-            int chave = Integer.parseInt(url[1]);
-            BigInteger key1 = BigInteger.valueOf(chave);
-            map.remove(key1);
-        }
+                logger.info(map);
 
-        if (((LinkedList<Object>) queuef2).getFirst().toString().contains("limpar")) {
-            logger.info("mapa reinicializado");
-            map.clear();
-            queuef2.clear();
-            logger.info(map);
-            logger.info(queuef2);
+                logger.info(queue);
 
-        }
+            }
+
+            if (((LinkedList<Object>) queuef3).getFirst().toString().contains("update")) {
+                String[] url = ((LinkedList<Object>) queuef3).getFirst().toString().split("/");
+
+                int chave = Integer.parseInt(url[1]);
+                BigInteger key1 = BigInteger.valueOf(chave);
+                map.remove(key1);
+                map.put(key1, url[2]);
+            }
+
+
+            if (((LinkedList<Object>) queuef3).getFirst().toString().contains("delete")) {
+                String[] url = ((LinkedList<Object>) queuef3).getFirst().toString().split("/");
+
+                int chave = Integer.parseInt(url[1]);
+                BigInteger key1 = BigInteger.valueOf(chave);
+                map.remove(key1);
+            }
+
+            if (((LinkedList<Object>) queuef3).getFirst().toString().contains("limpar")) {
+                logger.info("mapa reinicializado");
+                map.clear();
+                queuef3.clear();
+                logger.info(map);
+                logger.info(queuef3);
+
+            }
 
     }
 
